950fcabc7c88574a1926196c1b89882063ec606b
damienbod <damien_bod@hotmail.com>
2015-06-12 14:04:15 +0200

    test

--- dottilottiweb/Startup.cs
+++ dottilottiweb/Startup.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,7 +8,7 @@ using Microsoft.AspNet.Http;
 using Microsoft.AspNet.Routing;
 using Microsoft.Framework.DependencyInjection;
 
-namespace AspNet5ThemesSassGrunt
+namespace testyo
 {
     public class Startup
     {
@@ -16,16 +16,26 @@ namespace AspNet5ThemesSassGrunt
         {
         }
 
+        // This method gets called by a runtime.
+        // Use this method to add services to the container
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            // Uncomment the following line to add Web API services which makes it easier to port Web API 2 controllers.
+            // You will also need to add the Microsoft.AspNet.Mvc.WebApiCompatShim package to the 'dependencies' section of project.json.
+            // services.AddWebApiConventions();
         }
 
+        // Configure is called after ConfigureServices is called.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            // Configure the HTTP request pipeline.
             app.UseStaticFiles();
-            app.UseMvc();
 
+            // Add MVC to the request pipeline.
+            app.UseMvc();
+            // Add the following route for porting Web API 2 controllers.
+            // routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
         }
     }
 }
