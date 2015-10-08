0e9cb212fe4e2600b86e80a1b427a3c5a6bfe602
damienbod <damien_bod@hotmail.com>
2015-10-08 12:05:31 +0200

    adding the details page

---
+++ dottilottiweb/app/controllers/KartenDetailsController.js
@@ -0,0 +1,23 @@
+﻿(function () {
+	'use strict';
+
+	var module = angular.module('mainApp');
+
+	// this code can be used with uglify
+	module.controller('KartenDetailsController',
+		[
+			'$scope',
+			'$log',
+            '$stateParams',
+			KartenDetailsController
+		]
+	);
+
+	function KartenDetailsController($scope, $log, $stateParams) {
+
+	    $scope.karte = $stateParams.karte;
+	    $scope.kartenstyle = $stateParams.kartenstyle;
+
+	}
+
+})();
