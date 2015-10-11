5135b03a7accb0e75e9a93dcb88410f6cb0ba6cb
damienbod <damien_bod@hotmail.com>
2015-10-11 11:50:24 +0200

    adding details to cards

--- dottilottiweb/app/controllers/KartenDetailsController.js
+++ dottilottiweb/app/controllers/KartenDetailsController.js
@@ -21,87 +21,87 @@
 	function getKartenDetails(scope, karte) {
 	    switch (karte) {
 	        case "Dotti_Style_1":
-	            setKartenDetails(scope, "Dotti_Style_1", "Dotti_Style", "CHF 5.00");
+	            setKartenDetails(scope, "Dotti_Style_1", "Dotti_Style", "CHF 4.50");
 	            break;
 	        case "Dotti_Style_2":
-	            setKartenDetails(scope, "Dotti_Style_2", "Dotti_Style", "CHF 77.00");
+	            setKartenDetails(scope, "Dotti_Style_2", "Dotti_Style", "CHF 4.50");
 	            break;
 	        case "Dotti_Style_3":
-	            setKartenDetails(scope, "Dotti_Style_3", "Dotti_Style", "CHF 3.00");
+	            setKartenDetails(scope, "Dotti_Style_3", "Dotti_Style", "CHF 4.50");
 	            break;
             case "Dotti_Style_4":
-	            setKartenDetails(scope, "Dotti_Style_4", "Dotti_Style", "CHF 59.00");
+                setKartenDetails(scope, "Dotti_Style_4", "Dotti_Style", "CHF 4.50");
 	            break;
             case "Dotti_Style_5":
-	            setKartenDetails(scope, "Dotti_Style_5", "Dotti_Style", "CHF 34.00");
+                setKartenDetails(scope, "Dotti_Style_5", "Dotti_Style", "CHF 4.50");
 	            break;
 
 	        case "Mandala_Style_1":
-	            setKartenDetails(scope, "Mandala_Style_1", "Mandala_Style", "CHF 1.00");
+	            setKartenDetails(scope, "Mandala_Style_1", "Mandala_Style", "CHF 4.50");
 	            break;
 	        case "Mandala_Style_2":
-	            setKartenDetails(scope, "Mandala_Style_2", "Mandala_Style", "CHF 1.00");
+	            setKartenDetails(scope, "Mandala_Style_2", "Mandala_Style", "CHF 4.50");
 	            break;
 	        case "Mandala_Style_3":
-	            setKartenDetails(scope, "Mandala_Style_3", "Mandala_Style", "CHF 1.00");
+	            setKartenDetails(scope, "Mandala_Style_3", "Mandala_Style", "CHF 4.50");
 	            break;
 	        case "Mandala_Style_4":
-	            setKartenDetails(scope, "Mandala_Style_4", "Mandala_Style", "CHF 1.00");
+	            setKartenDetails(scope, "Mandala_Style_4", "Mandala_Style", "CHF 4.50");
 	            break;
 	        case "Mandala_Style_5":
-	            setKartenDetails(scope, "Mandala_Style_5", "Mandala_Style", "CHF 1.00");
+	            setKartenDetails(scope, "Mandala_Style_5", "Mandala_Style", "CHF 4.50");
 	            break;
 	        case "Mandala_Style_6":
-	            setKartenDetails(scope, "Mandala_Style_6", "Mandala_Style", "CHF 1.00");
+	            setKartenDetails(scope, "Mandala_Style_6", "Mandala_Style", "CHF 4.50");
 	            break;
 	        case "Mandala_Style_7":
-	            setKartenDetails(scope, "Mandala_Style_7", "Mandala_Style", "CHF 1.00");
+	            setKartenDetails(scope, "Mandala_Style_7", "Mandala_Style", "CHF 4.50");
 	            break;
 	        case "Mandala_Style_8":
-	            setKartenDetails(scope, "Mandala_Style_8", "Mandala_Style", "CHF 1.00");
+	            setKartenDetails(scope, "Mandala_Style_8", "Mandala_Style", "CHF 4.50");
 	            break;
 
 	        case "India_Style_1":
-	            setKartenDetails(scope, "India_Style_1", "India_Style", "CHF 2.00");
+	            setKartenDetails(scope, "India_Style_1", "India_Style", "CHF 4.50");
 	            break;
 	        case "India_Style_2":
-	            setKartenDetails(scope, "India_Style_2", "India_Style", "CHF 2.00");
+	            setKartenDetails(scope, "India_Style_2", "India_Style", "CHF 4.50");
 	            break;
 	        case "India_Style_3":
-	            setKartenDetails(scope, "India_Style_3", "India_Style", "CHF 2.00");
+	            setKartenDetails(scope, "India_Style_3", "India_Style", "CHF 4.50");
 	            break;
 	        case "India_Style_4":
-	            setKartenDetails(scope, "India_Style_4", "India_Style", "CHF 2.00");
+	            setKartenDetails(scope, "India_Style_4", "India_Style", "CHF 4.50");
 	            break;
 	        case "India_Style_5":
-	            setKartenDetails(scope, "India_Style_5", "India_Style", "CHF 2.00");
+	            setKartenDetails(scope, "India_Style_5", "India_Style", "CHF 4.50");
 	            break;
 	        case "India_Style_6":
-	            setKartenDetails(scope, "India_Style_6", "India_Style", "CHF 2.00");
+	            setKartenDetails(scope, "India_Style_6", "India_Style", "CHF 4.50");
 	            break;
 	        case "India_Style_7":
-	            setKartenDetails(scope, "India_Style_7", "India_Style", "CHF 2.00");
+	            setKartenDetails(scope, "India_Style_7", "India_Style", "CHF 4.50");
 	            break;
 	        case "India_Style_8":
-	            setKartenDetails(scope, "India_Style_8", "India_Style", "CHF 2.00");
+	            setKartenDetails(scope, "India_Style_8", "India_Style", "CHF 4.50");
 	            break;
 	        case "India_Style_9":
-	            setKartenDetails(scope, "India_Style_9", "India_Style", "CHF 2.00");
+	            setKartenDetails(scope, "India_Style_9", "India_Style", "CHF 4.50");
 	            break;
 	        case "India_Style_10":
-	            setKartenDetails(scope, "India_Style_10", "India_Style", "CHF 2.00");
+	            setKartenDetails(scope, "India_Style_10", "India_Style", "CHF 4.50");
 	            break;
 	        case "India_Style_11":
-	            setKartenDetails(scope, "India_Style_11", "India_Style", "CHF 2.00");
+	            setKartenDetails(scope, "India_Style_11", "India_Style", "CHF 4.50");
 	            break;
 	        case "India_Style_12":
-	            setKartenDetails(scope, "India_Style_12", "India_Style", "CHF 2.00");
+	            setKartenDetails(scope, "India_Style_12", "India_Style", "CHF 4.50");
 	            break;
 	        case "India_Style_13":
-	            setKartenDetails(scope, "India_Style_13", "India_Style", "CHF 2.00");
+	            setKartenDetails(scope, "India_Style_13", "India_Style", "CHF 4.50");
 	            break;
 	        case "India_Style_14":
-	            setKartenDetails(scope, "India_Style_14", "India_Style", "CHF 2.00");
+	            setKartenDetails(scope, "India_Style_14", "India_Style", "CHF 4.50");
 	            break;
 
 
@@ -109,11 +109,29 @@
 
 	};
 
+
 	function setKartenDetails(scope, karte, kartenstyle, price) {
 	    scope.karte = karte;
 	    scope.kartenstyle = kartenstyle;
-        scope.price = price
+	    scope.price = price;
+	    setKartenDetailsTitle(scope, karte, kartenstyle);
+	    setArticleNumber(scope, karte, kartenstyle);
 	};
 	
+	function setKartenDetailsTitle(scope, karte, kartenstyle) {
+	    if (kartenstyle = "Mandala_Style") {
+	        scope.title = "Mandala Style";
+	    }
+	    else if (kartenstyle = "Dotti_Style") {
+	        scope.title = "Stoffkarte Dotti Style";
+	    }
+	    else if (kartenstyle = "India_Style") {
+	        scope.title = "Stoffkarte India Style";
+	    }    
+	};
+
+    function setArticleNumber(scope, karte, kartenstyle) {
+	    scope.kartenArticleDesplay = kartenstyle.replace(/ /g, "_");
+	}
 
 })();
